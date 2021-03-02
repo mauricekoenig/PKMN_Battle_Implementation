c598ee150155467f79acd694693a1aedf8a4e4d4
mauricekoenig <60366053+mauricekoenig@users.noreply.github.com>
2021-03-02 22:56:55 +0100

    Update DamageCalculator.cs

--- Pokemon/DamageCalculator.cs
+++ Pokemon/DamageCalculator.cs
@@ -1,77 +1,133 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public static class DamageCalculator
 {
-    public static IDamageReducingEffect ClientActives;
-    public static IDamageReducingEffect HostActives;
+    private static Weather CurrentWeather;
+    public static List<IDamageReducingEffect> HostDamageReductions = new List<IDamageReducingEffect>();
+    public static List<IDamageReducingEffect> ClientDamageReductions = new List<IDamageReducingEffect>();
 
-    public const int DoubleHit = 1;
-    public const int Burn = 1;
-    public const int Reducing = 1;
-    public const int WeatherBoost = 1;
-    public const int OtherBoostingSources = 1;
-    public const int CriticalHit = 1;
-    public const int DamageBoostingItem = 1;
-    public const int RandomGeneratedFactor = 85;
-    public const int BerryOrExpertBelt = 1;
+    #region FactorModifier
+    private const double MultiTargetModifier = 1;
+    private static double BurnModifier = 1;
+    private static double DamageReductionModifier = 1;
+    private static double WeatherModifier = 1;
+    private static double StatChargedModifier = 1;
+    private static double CriticalHitModifier = 1;
+    private static double ItemModifier = 1;
+    private static double RollModifier = 100;
+    private static double BerryOrExpBeltModifier = 1;
+    #endregion
 
-    public static int Attack (PokemonMove attackingMove, Pokemon attacker, Pokemon defender)
+    public static int GetDamage (PokemonMove attackingMove, Pokemon attacker, Pokemon defender)
     {
         var Attacker = attacker;
         var Defender = defender;
         var Move = attackingMove;
         var MoveCategory = attackingMove.moveCategory;
 
+        SpecifyWeatherInfluence(Move.moveType);
         if (MoveCategory == PokemonMoveCategory.Physical)
         {
             double value = 0;
-            int F1 = Burn * Reducing * WeatherBoost * OtherBoostingSources;
+            double F1 = BurnModifier * DamageReductionModifier * WeatherModifier * StatChargedModifier;
 
             value = 100 * 2;
             value = Math.Floor(value / 5);
             value = Math.Floor(value + 2);
             value = Math.Floor(value * Move.moveBasePower);
-            value = Math.Floor(value * attacker.PhysicalAttack);
-            value = Math.Floor(value / defender.PhysicalDefense);
+            value = Math.Floor(value * Attacker.PhysicalAttack);
+            value = Math.Floor(value / Defender.PhysicalDefense);
             value = Math.Floor(value / 50);
             value = Math.Floor(value + 2);
 
             value = Math.Floor(value * F1);
-            value = Math.Floor(value * CriticalHit);
-            value = Math.Floor(value * DamageBoostingItem);
+            value = Math.Floor(value * CriticalHitModifier);
+            value = Math.Floor(value * ItemModifier);
             value = Math.Floor(value * Testing.x.roll);
             value = Math.Floor(value / 100);
             value = Math.Floor(value * TypeUtility.GetSTABMultiplier(Move, Attacker));
             value = Math.Floor(value * TypeUtility.GetEffectiveMultiplier(Move, Defender));
-            value = Math.Floor(value * BerryOrExpertBelt);
+            value = Math.Floor(value * BerryOrExpBeltModifier);
+
+            ResetModifier();
             return (int)Math.Floor(value);
         }
         else if (MoveCategory == PokemonMoveCategory.Special)
         {
             double value = 0;
-            int F1 = 1 * Reducing * WeatherBoost * OtherBoostingSources;
+            double F1 = 1 * DamageReductionModifier * WeatherModifier * StatChargedModifier;
 
             value = 100 * 2;
             value = Math.Floor(value / 5);
             value = Math.Floor(value + 2);
             value = Math.Floor(value * Move.moveBasePower);
-            value = Math.Floor(value * attacker.SpecialAttack);
-            value = Math.Floor(value / defender.SpecialDefense);
+            value = Math.Floor(value * Attacker.SpecialAttack);
+            value = Math.Floor(value / (Defender.SpecialDefense * GetSandMultiplier(Defender)) );
             value = Math.Floor(value / 50);
             value = Math.Floor(value + 2);
 
             value = Math.Floor(value * F1);
-            value = Math.Floor(value * CriticalHit);
-            value = Math.Floor(value * DamageBoostingItem);
+            value = Math.Floor(value * CriticalHitModifier);
+            value = Math.Floor(value *ItemModifier);
             value = Math.Floor(value * Testing.x.roll);
             value = Math.Floor(value / 100);
             value = Math.Floor(value * TypeUtility.GetSTABMultiplier(Move, Attacker));
             value = Math.Floor(value * TypeUtility.GetEffectiveMultiplier(Move, Defender));
-            value = Math.Floor(value * BerryOrExpertBelt);
+            value = Math.Floor(value * BerryOrExpBeltModifier);
+
+            ResetModifier();
             return (int)Math.Floor(value);
         }
-
         else return -1000;
+    }
+    public static Weather GetWeather ()
+    {
+        return CurrentWeather;
+    }
+    private static double GetSandMultiplier(Pokemon pokemon)
+    {
+        if (GetWeather() != Weather.Sand) return 1;
+
+        var firstType = pokemon.firstType;
+        var secondType = pokemon.secondType;
+
+        if (firstType == PokemonType.Rock || secondType == PokemonType.Rock)
+        {
+            return 1.5;
+        }
+
+        else return 1;
+
+    }
+    public static void SetWeather(Weather newWeather)
+    {
+        CurrentWeather = newWeather;
+    }
+    private static void SpecifyWeatherInfluence (PokemonType attackType)
+    {
+        if (GetWeather() == Weather.Sun)
+        {
+            if (attackType == PokemonType.Fire)
+            {
+                WeatherModifier = 1.5;
+            }
+        }
+
+        if (GetWeather() == Weather.Rain)
+        {
+            if (attackType == PokemonType.Water)
+            {
+                WeatherModifier = 1.5;
+            }
+        }
+    }
+    private static void ResetModifier ()
+    {
+        WeatherModifier = 1;
+        StatChargedModifier = 1;
+        DamageReductionModifier = 1;
+        BurnModifier = 1;
     }
 }
